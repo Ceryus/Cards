74078854b8bae6e15decc0a625beeec50c1eddb4
100156522 <100156522@LHS1203-15.lubbock.isd>
2017-02-22 14:42:20 -0600

    Committing Cards.

--- src/CardTester.java
+++ src/CardTester.java
@@ -1,3 +1,4 @@
+
 /**
  * This is a class that tests the Card class.
  */
@@ -8,6 +9,14 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
+		Card card1 = new Card("Queen", "Spade",10);
+		Card card2 = new Card("Jack", "Heart",10);
+		Card card3 = new Card("Jack", "Heart",10);
+		System.out.println("Card1:"+card1);
+		System.out.println("Card2:"+card2);
+		System.out.println("Card3:"+card3);
+		System.out.println(card1.matches(card2));
+		System.out.println(card2.matches(card3));
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
 	}
 }
